07196f6da14d3774ca1be3a2ddf698964cfa6a99
Devng Langhanoja <dclanghnoja@gmail.com>
2022-11-18 12:53:33 +0530

    setup done for fronend and backend

---
+++ backend/src/utils/messages.js
@@ -0,0 +1,26 @@
+class Messages {
+    static GET_USERS = "User get succsessfully!";
+    static USER_NOT_FOUND_WITH_EMAIL =
+        "Can not found the user with this email.";
+    static LOGIN_SUCCESSFUL = "Login succsessfully done!";
+    static RESIGTER_SUCCESSFUL = "Registration successfully done!";
+    static WRONG_LOGIN = "Email Id or Password is Wrong!";
+    static FORGET_PASSWORD_DONE = "OTP is sent to your emali, please verify and changes your password as soon as possible!"
+    static USER_NOT_ACTIVE = "User is not active";
+    static SOMETIN_WENT_WRONG = "Someting went wrong, Please try again!!";
+    static EMAIL_EXISTS = "This email is already exits. Please use another!!";
+    static EMAIL_NOT_EXISTS = "Please enter valid email address!"
+    static EMAIL_TEMPLATE_ADDED = "Your template is added!!";
+    static EMAIL_TEMPLATE_EDITED = "Your template has been updated!!";
+    static EMAIL_TEMPLATE_DELETE = "Your template is deleted!!";
+    static EMAIL_TEMPLATE_EXISTS = "The Template you're trying to add the already exists, Please use another!"
+    static WRONG_OTP = "Provided OTP is wrong!!"
+    static OTP_EXPIRED = "Provide OTP is expired!!"
+    static OTP_VERIFIED = "Your OTP has been verified!!"
+    static PASSWORD_SAME = "Password should not be same as old, Please enter another password!!"
+    static PASSWORD_CHANGED = "Your password had been changed!!"
+    static SUBSCRIPTIONDONE = "You have succuessfully subscribed to the nacscan news letter!!"
+    static SUBSCRIPTIONALLREADY = "You have already subscribed to nacscan news letter feature!!"
+}
+
+module.exports = Messages
